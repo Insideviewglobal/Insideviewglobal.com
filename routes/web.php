d954702e3311b831efccbd9a0ca4cff00b49ba38
Zachary Oakes <zachary.oakes@gmail.com>
2023-10-21 12:56:32 +0900

    setup landing
    feat(ui): the index page is changed to the landing page view.

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome');
+Route::view('/', 'pages.landing');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
