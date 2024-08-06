3358013083607ad6fa87d38b497aeac04a47a7a4
Luis Juarez <luisn.juarez@gmail.com>
2024-08-06 01:08:55 -0700

    add storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import alpinejs from "@astrojs/alpinejs";
-
+import storyblok from '@storyblok/astro';
 import icon from "astro-icon";
+import { loadEnv } from 'vite';
+
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,5 +23,22 @@ export default defineConfig({
     drafts: true
   },
   site: 'https://www.insideviewglobal.com',
-  integrations: [tailwind(), sitemap(), mdx(), alpinejs(), icon()]
+  integrations: [tailwind(), sitemap(), mdx(), alpinejs(), icon(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        // Add your components here
+        blogPost: 'storyblok/BlogPost',
+        page: 'storyblok/Page',
+        feature: 'storyblok/Feature',
+       grid: 'storyblok/Grid',
+        teaser: 'storyblok/Teaser',
+        RichBodyContent: 'storyblok/RichBodyContent',
+      },
+      apiOptions: {
+        // Choose your Storyblok space region
+        region: 'ca', // optional,  or 'eu' (default)
+      },
+    }),
+  ]
 });
