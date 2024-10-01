d84815c2ed9b7b5ea626f25fdf16c28b24006488
Andres <lopez.andresrigoberto@gmail.com>
2024-10-01 15:58:46 -0700

    add: rss feed file

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,17 +1,37 @@
-import rss from "@astrojs/rss";
-import { getCollection } from "astro:content";
-export async function get() {
+// import rss from "@astrojs/rss";
+// import { getCollection } from "astro:content";
+// export async function get() {
+//   const posts = await getCollection('posts');
+//   return rss({
+//     title: 'Astro Learner | Blog',
+//     description: 'My journey learning Astro',
+//     site: 'https://my-blog-site.netlify.app',
+//     items: posts.map((post) => ({
+//       title: post.data.title,
+//       pubDate: post.data.pubDate,
+//       description: post.data.description,
+//       link: `/posts/${post.slug}/`,
+//     })),
+//     customData: `<language>en-us</language>`,
+//     stylesheet: '/rss/styles.xsl',
+//   });
+// }
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
   const posts = await getCollection('posts');
   return rss({
-    title: 'Astro Learner | Blog',
-    description: 'My journey learning Astro',
-    site: 'https://my-blog-site.netlify.app',
+    title: 'Inside View Global Site',
+    description: 'Inside View Global Site',
+    site: context.site,
     items: posts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
       link: `/posts/${post.slug}/`,
     })),
-    customData: `<language>en-us</language>`,
+    customData: `<language>en</language>`,
+    stylesheet: '/rss/styles.xsl',
   });
 }
