f95dbe7ad2b174ebc886d8c8e0b3f475d5b53804
Luis Juarez <luisn.juarez@gmail.com>
2024-10-13 21:46:38 -0700

    udpating the product pages

--- src/types/types.ts
+++ src/types/types.ts
@@ -8,8 +8,10 @@ export interface Feature {
 export interface Product {
   product_id: string;
   product_title: string;
+  product_title_html: string;
   short_description: string;
   full_description: string;
+  product_message:string;
   image: string;
   features_title: string;
   features_description: string;
